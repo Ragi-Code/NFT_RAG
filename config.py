79223f362df0964001c6af3b0131a37968b1bcf1
Ragi-Code <84683214+Ragi-Code@users.noreply.github.com>
2022-01-06 20:26:54 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,37 @@
+CONFIG = [
+    {
+        'id': 1,
+        'name': 'background',
+        'directory': 'background',
+        'required': True,
+        'rarity_weights': None,
+    },
+    {
+        'id': 2,
+        'name': 'foxes',
+        'directory': 'foxes',
+        'required': True,
+        'rarity_weights': None
+    },
+    {
+        'id': 3,
+        'name': 'eyes',
+        'directory': 'eyes',
+        'required': True,
+        'rarity_weights': None
+    },
+    {
+        'id': 4,
+        'name': 'shirt',
+        'directory': 'shirt',
+        'required': True,
+        'rarity_weights': [12, 15, 17, 10, 10, 7, 20, 15, 15, 10]
+    },
+    {
+        'id': 5,
+        'name': 'hat',
+        'directory': 'hat',
+        'required': True,
+        'rarity_weights': [15, 15, 15, 10, 10, 20, 15]
+    }
+]
